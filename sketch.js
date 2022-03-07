100309761461dc7df44e33c8c3fe4c3cd8fab270
EndlessEnnui <Jaymianbustos927@gmail.com>
2022-03-06 19:01:59 -0800

    Work

---
+++ sketch.js
@@ -0,0 +1,11 @@
+let t = "Let this be a temporary placeholder for my data curator"
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  fill(0);
+
+  text(t, 150, 150, 70, 70);
+}
