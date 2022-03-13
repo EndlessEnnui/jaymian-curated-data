4553eb2ce8bf4d710a12c69f13fd5c7d1c410fc4
EndlessEnnui <Jaymianbustos927@gmail.com>
2022-03-12 17:11:28 -0800

    Slot Machine Update!

--- sketch.js
+++ sketch.js
@@ -1,11 +1,46 @@
-let t = "Let this be a temporary placeholder for my data curator"
+
+
+slots = [];
+
+function preload(){
+  slots[0]= loadImage("images/1.png");
+  slots[1]= loadImage("images/2.png");
+  slots[2]= loadImage("images/3.png");
+  slots[3]= loadImage("images/4.png");
+  sm = loadImage("images/5.png");
+}
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(1100, windowHeight);
+  background(200, 255, 255);
+
 }
 
 function draw() {
-  background(220);
-  fill(0);
 
-  text(t, 150, 150, 70, 70);
+  fill(0);
+  image(sm, 150, -60);
+
+
+}
+function mouseClicked() {
+
+  background(0, 5, 5)
+
+  slotResult1 = int(random(slots.length));
+  slotResult2 = int(random(slots.length));
+  slotResult3 = int(random(slots.length));
+
+
+  image(slots[slotResult1], 310,430);
+  image(slots[slotResult2], 470,430);
+  image(slots[slotResult3], 630,430);
+
+
+  console.log(slotResult1);
+  console.log(slotResult2);
+  console.log(slotResult2);
+}
+function windowResized() {
+	resizeCanvas(windowWidth, windowHeight);
 }
