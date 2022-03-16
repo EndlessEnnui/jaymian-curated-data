36e439ee6fe48a25bf2655a01b4431f86b7b3d13
EndlessEnnui <Jaymianbustos927@gmail.com>
2022-03-16 01:22:57 -0700

    A whole bunch of shit

--- sketch.js
+++ sketch.js
@@ -1,6 +1,12 @@
 
 
 slots = [];
+let slotmachine;
+let display;
+
+let t = "Pull the lever to start!"
+
+
 
 function preload(){
   slots[0]= loadImage("images/1.png");
@@ -8,39 +14,58 @@ function preload(){
   slots[2]= loadImage("images/3.png");
   slots[3]= loadImage("images/4.png");
   sm = loadImage("images/5.png");
+  cg = loadImage("images/6.png");
+  bg = loadImage("images/Pixel Casino.jpg")
+  tb = loadImage("images/TextBox.png")
+  myFont = loadFont("font/PressStart2P-Regular.ttf")
 }
 
 function setup() {
-  createCanvas(1100, windowHeight);
-  background(200, 255, 255);
+  createCanvas(windowWidth, windowHeight);
+  background(bg, 5, 5, 100, 100);
+  imageMode(CENTER);
 
 }
 
 function draw() {
 
-  fill(0, 5, 5);
-  image(sm, 150, -60);
+  fill(0, 0, 0);
 
-
+  image(sm, width*0.5, height*0.5, width*0.325, height);
+  image(cg, width*0.8, windowHeight/2.3, width*0.3, height*0.6);
+image(tb, windowWidth/1.4, windowHeight/1.2, windowWidth/2.1, windowHeight/1.9 );
+textFont(myFont);
+textSize(50);
+text(t, windowWidth/1.9, windowHeight/1.3, windowWidth/2.1, windowHeight/1.9);
 }
 function mouseClicked() {
 
-  background(0, 5, 5);
 
+  fill(0, 0, 0);
+rect(windowWidth*0.4,windowHeight*0.35, windowWidth*0.2, windowHeight*0.17);
   slotResult1 = int(random(slots.length));
   slotResult2 = int(random(slots.length));
   slotResult3 = int(random(slots.length));
 
 
-  image(slots[slotResult1], 310,430);
-  image(slots[slotResult2], 470,430);
-  image(slots[slotResult3], 630,430);
-
+  image(slots[slotResult1], width*0.435, height*0.43, width*0.06, height*0.08);
+  image(slots[slotResult2], width*0.5, height*0.43, width*0.06, height*0.08);
+  image(slots[slotResult3], width*0.565, height*0.43, width*0.06, height*0.08);
 
   console.log(slotResult1);
   console.log(slotResult2);
   console.log(slotResult2);
+
+
+
 }
 function windowResized() {
 	resizeCanvas(windowWidth, windowHeight);
+  if (phase == 'display') {
+  display();
+} else if (phase == 'playing') {
+  slotmachine();
+  randomtext();
+}
+textResize();
 }
